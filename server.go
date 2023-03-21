978f3b5b7331d0f59c7d9136909bd6fad86b2e7e
PedroHenriqueDevBR <p.lyma18@gmail.com>
2023-03-21 08:54:11 -0300

    chore: added comments

--- server.go
+++ server.go
@@ -5,18 +5,22 @@ import "github.com/gofiber/fiber/v2"
 func main() {
 	app := fiber.New()
 
+	// Return all users
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Lista de usuarios")
 	})
 
+	// Validate and save user
 	app.Post("/", func(c *fiber.Ctx) error {
 		return c.SendString("Usuario cadastrado com sucesso")
 	})
 
+	// search and update user
 	app.Put("/:id", func(c *fiber.Ctx) error {
 		return c.SendString("Usuario " + c.Params("id") + " atualizado com sucesso")
 	})
 
+	// search and delete user
 	app.Delete("/:id", func(c *fiber.Ctx) error {
 		return c.SendString("Usuario " + c.Params("id") + " removido com sucesso")
 	})
